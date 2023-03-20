51eb4214d25a080c41df5a2fb059fd266a8732c7
Gennady Umikashvili <gennadyumikashvili@yandex.ru>
2023-03-20 20:11:23 +0300

    ver.2

--- yatube_api/posts/models.py
+++ yatube_api/posts/models.py
@@ -5,37 +5,42 @@ User = get_user_model()
 
 
 class Group(models.Model):
-    title = models.CharField('Заглавие', max_length=200)
-    slug = models.SlugField('Значение', unique=True)
-    description = models.TextField('Описание')
+    title = models.CharField("Заглавие", max_length=200)
+    slug = models.SlugField("Значение", unique=True)
+    description = models.TextField("Описание")
 
     class Meta:
-        verbose_name = 'Группа'
-        verbose_name_plural = 'Группы'
+        verbose_name = "Группа"
+        verbose_name_plural = "Группы"
 
     def __str__(self):
         return self.title
 
 
 class Post(models.Model):
-    text = models.TextField('Текст')
-    pub_date = models.DateTimeField(
-        'Дата публикации', auto_now_add=True
-    )
+    text = models.TextField("Текст")
+    pub_date = models.DateTimeField("Дата публикации", auto_now_add=True)
     author = models.ForeignKey(
-        User, on_delete=models.CASCADE, related_name='posts', verbose_name='Автор'
+        User,
+        on_delete=models.CASCADE,
+        related_name="posts",
+        verbose_name="Автор",
     )
     image = models.ImageField(
-        upload_to='posts/', blank=True, verbose_name='Картинка'
+        upload_to="posts/", blank=True, verbose_name="Картинка"
     )  # поле для картинки
     group = models.ForeignKey(
-        Group, on_delete=models.SET_NULL,
-        related_name='posts', blank=True, null=True, verbose_name='Группа'
+        Group,
+        on_delete=models.SET_NULL,
+        related_name="posts",
+        blank=True,
+        null=True,
+        verbose_name="Группа",
     )
 
     class Meta:
-        verbose_name = 'Пост'
-        verbose_name_plural = 'Посты'
+        verbose_name = "Пост"
+        verbose_name_plural = "Посты"
 
     def __str__(self):
         return self.text
@@ -43,16 +48,22 @@ class Post(models.Model):
 
 class Comment(models.Model):
     author = models.ForeignKey(
-        User, on_delete=models.CASCADE, related_name='comments', verbose_name='Автор'
+        User,
+        on_delete=models.CASCADE,
+        related_name="comments",
+        verbose_name="Автор",
     )
     post = models.ForeignKey(
-        Post, on_delete=models.CASCADE, related_name='comments', verbose_name='Пост'
+        Post,
+        on_delete=models.CASCADE,
+        related_name="comments",
+        verbose_name="Пост",
     )
-    text = models.TextField('Текст')
+    text = models.TextField("Текст")
     created = models.DateTimeField(
-        'Дата добавления', auto_now_add=True, db_index=True
+        "Дата добавления", auto_now_add=True, db_index=True
     )
 
     class Meta:
-        verbose_name = 'Комментарий'
-        verbose_name_plural = 'Комментарии'
+        verbose_name = "Комментарий"
+        verbose_name_plural = "Комментарии"
